60ee33ed3a928210c945f9c1d8243acf08048c57
incentivemohit <mohitkumar.e21@nsut.ac.in>
2024-02-03 20:35:55 +0530

    update

--- index.js
+++ index.js
@@ -5,20 +5,15 @@ const PORT = process.env.PORT || 8000;
 const connection = require("./db/config");
 const saveData = require("./routes/saveData");
 const payment = require("./routes/payment");
-const path  = require("path")
+const path = require("path");
 const cors = require("cors");
 connection();
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static("files"));
-app.use("/", express.static(path.join(__dirname,"public/build")));
 
 app.use("/api/v1/details", saveData);
 app.use("/api/v1/payment", payment);
-
-app.get("/",(req,res)=>{
-  res.sendFile(path.join(__dirname,"public/build/index.html"))
-})
 
 app.listen(PORT, (err) => {
   if (err) {
