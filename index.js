c2bbdc5fcb8cfdd4ce252f195112b9fc6bdb9767
incentivemohit <mohitkumar.e21@nsut.ac.in>
2024-02-03 21:52:25 +0530

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+  res.sendFile(path.join(__dirname, "public/build/index.html"));
 });
 
 app.listen(PORT, (err) => {
