984243afb886fda9f5aa450aa1d5a764397d2e73
Mohit Kumar <86621804+mohitkumar5976@users.noreply.github.com>
2024-02-11 17:12:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,47 @@ const PORT = process.env.PORT || 8000;
 const connection = require("./db/config");
 const saveData = require("./routes/saveData");
 const payment = require("./routes/payment");
-const path = require("path");
+const SaveDetailsModel = require("./models/saveData");
 const cors = require("cors");
 connection();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", , "http://localhost:3001"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/files", express.static("files"));
 
 app.use("/api/v1/details", saveData);
 app.use("/api/v1/payment", payment);
 
-app.listen(PORT, (err) => {
+const server = require("http").createServer(app);
+
+const io = require("./util/socket").init(server);
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("getData", async (data) => {
+    try {
+      const orders = await SaveDetailsModel.find({ userId: data.userId });
+
+      socket.emit("receiveData", {
+        orders,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("client disconnected");
+  });
+});
+
+server.listen(PORT, (err) => {
   if (err) {
     console.log(err.message);
   } else {
