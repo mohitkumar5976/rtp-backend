77f4117c1d8980c785a1ea4e5e908875196ec701
Mohit Kumar <86621804+mohitkumar5976@users.noreply.github.com>
2024-02-11 16:54:19 +0530

    Update saveData.js

--- controllers/saveData.js
+++ controllers/saveData.js
@@ -1,4 +1,5 @@
 const SaveDetailsModel = require("../models/saveData");
+const io = require("../util/socket");
 exports.saveData = async (req, res) => {
   try {
     const {
@@ -12,10 +13,9 @@ exports.saveData = async (req, res) => {
       payment_ID,
       amount,
     } = req.body;
-    const docUrl = `https://${
-      req.headers.host
-    }/${req.file.path}`;
+    const docUrl = `https://${req.headers.host}/${req.file.path}`;
     const savedData = new SaveDetailsModel({
+      userId: "123",
       docUrl,
       phoneNo,
       noOfPages,
@@ -32,6 +32,7 @@ exports.saveData = async (req, res) => {
     await savedData.save();
 
     const printableData = {
+      userId: savedData.userId,
       id: savedData._id,
       docUrl: savedData.docUrl,
       phoneNo: savedData.phoneNo,
@@ -46,7 +47,11 @@ exports.saveData = async (req, res) => {
       amount: savedData.amount,
     };
 
-    return res.status(200).json({ printableData, message: "success" });
+    io.getIO().emit("addOrder", {
+      printableData
+    })
+
+     res.status(200).json({ printableData, message: "success" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
