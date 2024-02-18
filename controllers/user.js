ad50ac0318fd31bb3dd27a5025f12e53dff40a60
Mohit Kumar <86621804+mohitkumar5976@users.noreply.github.com>
2024-02-18 19:39:14 +0530

    Add files via upload

---
+++ controllers/user.js
@@ -0,0 +1,42 @@
+const User = require("../models/userModel");
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ status: 404, message: "User doesn't exist" });
+    }
+
+    if (user.password !== password) {
+      return res.json({ status: 404, message: "Password doesn't match" });
+    }
+
+    const userData = {
+      _id: user._id,
+      email: user.email,
+    };
+
+    return res.json({ userData, status: 200, message: "login successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.checkShopId = async (req, res) => {
+  try {
+    const { shopId } = req.body;
+
+    let shopkeeperId = await User.findById(shopId);
+
+    if (!shopkeeperId) {
+      return res.json({ status: 404, message: "ShopId doesn't exist" });
+    }
+
+    return res.json({ status: 200, message: "ShopId Exists" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
