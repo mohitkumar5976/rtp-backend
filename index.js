88f1d631b67f018e4a67b4f3f6b6cc81940237a4
Mohit Kumar <86621804+mohitkumar5976@users.noreply.github.com>
2024-02-23 15:19:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connection();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
-
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token");
   next();
 });
 
